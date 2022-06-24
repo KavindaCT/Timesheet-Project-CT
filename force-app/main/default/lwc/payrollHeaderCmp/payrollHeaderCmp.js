7cb88692aae1a55c0d108a71f61118c44eb2a74a
Narmatha-Jehanathan <narmijehan@gmail.com>
2022-06-24 12:46:49 +0530

    Added due date, layout and css changes

--- force-app/main/default/lwc/payrollHeaderCmp/payrollHeaderCmp.js
+++ force-app/main/default/lwc/payrollHeaderCmp/payrollHeaderCmp.js
@@ -1,8 +1,26 @@
 import { LightningElement } from 'lwc';
 
 export default class PayrollHeaderCmp extends LightningElement {
+    dueDate;
+    paymentDate;
 
-    viewSalary(){};
+    connectedCallback(){
+        const currentDate = new Date();
+        console.log(currentDate);
+        this.paymentDate = (new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 5)).toDateString().substring(4);
+        var lastDate = (new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0));
+        if(lastDate.getDay() <6 && lastDate.getDay() > 0){
+            this.dueDate = lastDate.toDateString().substring(4);
+        }else if(lastDate.getDay() == 6){
+            let lastPaymentdate = new Date(currentDate).setDate(lastDate.getDate()-1);
+            this.dueDate = new Date(lastPaymentdate).toDateString().substring(4);
+        }else{
+            let lastPaymentdate = new Date(currentDate).setDate(lastDate.getDate()-2);
+           this.dueDate = new Date(lastPaymentdate).toDateString().substring(4);
+        }
+    }
 
-    viewEarnings(){};
+    viewSalary(){}
+
+    viewEarnings(){}
 }
