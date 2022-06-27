de21501151d37f4994b52827e26a3825172b88e7
vidurasathsara99 <vidurasathsara40gmail.com>
2022-06-27 14:56:08 +0530

    added approval classes

--- force-app/main/default/lwc/timeSheetCmp/timeSheetCmp.js
+++ force-app/main/default/lwc/timeSheetCmp/timeSheetCmp.js
@@ -1,5 +1,6 @@
 import { LightningElement, api, wire } from 'lwc';
 import getTimesheetDays from '@salesforce/apex/TimesheetDataService.getTimesheetDays';
+import STATUS from '@salesforce/schema/Timesheet__c.Status__c';
 
 export default class TimeSheetCmp extends LightningElement {
     @api timePeriod;
@@ -7,6 +8,7 @@ export default class TimeSheetCmp extends LightningElement {
     activeWeek; // Only contains weekStart and weekEnding
     activeWeekNumber;
     openModal = false;
+    currentRecordId;
 
     @wire(getTimesheetDays, { timesheetId: '$timesheetId', weekNumber: '$activeWeekNumber' })
     timesheetDays;
@@ -14,6 +16,17 @@ export default class TimeSheetCmp extends LightningElement {
     handleClickSubmit(event){
         this.template.querySelector('c-heading-cmp').handleStatus(event.target.value);
         this.openModal = true;
+        // this.currentRecordId='a008d000005UjhoAAC';
+        // console.log('@@currentRecordId@@@'+this.currentRecordId);
+        // updateToggle({cliId: this.currentRecordId})
+        //   .then(() => {
+        //     console.log('SUCCESS');
+        //     return refreshApex(this.caseLineItems);
+        // })
+        // .catch((error) => {
+        //     this.errorMessage=error;
+        //     console.log('unable to update the record due to'+JSON.stringify(this.errorMessage));
+        // });
         console.log('Day data: ' + JSON.stringify(this.timesheetDays));
     }
 
