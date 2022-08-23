2c2b751eb0923a05a1703494a0f3acdfee335d6c
narmatha-jehanathan <narmijehan@gmail.com>
2022-08-23 07:48:56 +0530

    Approvalrequest lightning table component

---
+++ force-app/main/default/lwc/approvalRequest/approvalRequest.js
@@ -0,0 +1,34 @@
+import { api, LightningElement, wire } from 'lwc';
+import getTimesheetId from '@salesforce/apex/TimesheetDataService.getTimesheetId';
+
+
+const columns = [
+    { label: 'Name', fieldName: 'Name', type:'Text(80)'},
+    { label: 'Timesheet Date', fieldName: 'Timesheet_Date__c', type: 'Date' },
+    { label: 'Earning type', fieldName: 'Earning_Type__c', type: 'Picklist' },
+    { label: 'Hours', fieldName: 'Hours__c', type: 'Number(16, 2)' },
+    { label: 'Timesheet Month', fieldName: 'Timesheet_month__c', type: 'Formula (Text)' },
+    
+    //{ label: 'Timesheet Name', fieldName: 'Timesheet__r.name', type: 'Text(80)' }
+    //{ label: 'Timesheet Day', fieldName: 'Day__c', type: 'Picklist' }
+];
+
+export default class ApprovalRequest extends LightningElement {
+    @api recordId;
+
+    @wire(getTimesheetId,{recordId:'$recordId'})
+    timesheetData
+    // wireTimesheetId(results){
+
+    //     if(results){
+    //         let timesheetData = JSON.stringify(results);
+    //        // console.log('results '+JSON.stringify(results));
+    //         console.log('results data'+timesheetData);
+    //     }else{
+    //         console.log('error');
+    //     }
+    // }
+
+    //data = [];
+    columns = columns;
+}
